bc30dbfdf4a82e6dfed42905396fc1d71c729787
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-28 23:55:12 +0300

    ColumnScope & Row

--- app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
@@ -3,17 +3,17 @@ package com.metehanbolat.composelearningss
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.metehanbolat.composelearningss.ui.theme.ComposeLearningSSTheme
-import com.metehanbolat.composelearningss.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +22,30 @@ class MainActivity : ComponentActivity() {
             ComposeLearningSSTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    Column(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+                        CustomItem(weight = 1f)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ){}
 }
 
 @Preview(showBackground = true)
@@ -34,33 +53,13 @@ class MainActivity : ComponentActivity() {
 fun DefaultPreview() {
     ComposeLearningSSTheme {
         Column(
-            modifier = Modifier.fillMaxSize().padding(top = 10.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(50.dp),
-                color = MaterialTheme.colors.primary
-            ){}
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(50.dp),
-                color = MaterialTheme.colors.primary
-            ){}
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(50.dp),
-                color = MaterialTheme.colors.primary
-            ){}
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(50.dp),
-                color = MaterialTheme.colors.primary
-            ){}
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
