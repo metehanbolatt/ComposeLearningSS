b0cae2663fcbcdd4bbedfcf0307bce638d6b0c4e
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-28 23:40:32 +0300

    Column & Surface

--- app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
@@ -3,12 +3,15 @@ package com.metehanbolat.composelearningss
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.composelearningss.ui.theme.ComposeLearningSSTheme
 import com.metehanbolat.composelearningss.ui.theme.Typography
 
@@ -19,33 +22,45 @@ class MainActivity : ComponentActivity() {
             ComposeLearningSSTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.h5
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeLearningSSTheme {
-        Column() {
-            Greeting("Android")
-            CustomText(text = "Metehan")
+        Column(
+            modifier = Modifier.fillMaxSize().padding(top = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colors.primary
+            ){}
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colors.primary
+            ){}
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colors.primary
+            ){}
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colors.primary
+            ){}
         }
     }
 }
