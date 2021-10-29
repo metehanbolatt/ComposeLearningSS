bf5e5040c4ad23d96e49caf39a57ad39977446e0
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-29 16:19:33 +0300

    How can I use string resource in Text

--- app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,7 +40,7 @@ fun DefaultPreview() {
     ComposeLearningSSTheme {
         Column(modifier = Modifier.fillMaxSize()){
             Text(
-                text = "Hello World",
+                text = stringResource(id = R.string.app_name),
                 modifier = Modifier
                     //Background'u önce verirsek padding olmadan boyar.
                     //Background'u sonra verirsek sadece textin oldugu yeri boyar.
