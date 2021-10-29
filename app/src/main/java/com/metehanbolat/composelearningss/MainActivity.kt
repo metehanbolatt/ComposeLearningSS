3e1072cd45267e12d74cefa4f6cfeddafec9448e
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-29 16:29:00 +0300

    buildAnnotatedString & withStyle

--- app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/composelearningss/MainActivity.kt
@@ -15,6 +15,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,20 +40,50 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun CustomText() {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            //Background'u önce verirsek padding olmadan boyar.
+            //Background'u sonra verirsek sadece textin oldugu yeri boyar.
+            .background(MaterialTheme.colors.primary)
+            .padding(16.dp)
+            .background(MaterialTheme.colors.secondary)
+            .width(200.dp),
+        color = Color.White,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.End
+    )
+}
+
+@Composable
+fun CustomText2() {
+    Text(
+        buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)){
+                withStyle(style = SpanStyle(
+                    color = MaterialTheme.colors.primary,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )){
+                    append("A")
+                }
+                append("B")
+                append("C")
+                append("D")
+            }
+        }, modifier = Modifier.width(200.dp)
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeLearningSSTheme {
         Column(modifier = Modifier.fillMaxSize()){
-            Text(
-                text = stringResource(id = R.string.app_name),
-                modifier = Modifier
-                    //Background'u önce verirsek padding olmadan boyar.
-                    //Background'u sonra verirsek sadece textin oldugu yeri boyar.
-                    .background(MaterialTheme.colors.primary)
-                    .padding(16.dp)
-
-            )
+            CustomText2()
         }
     }
 }
